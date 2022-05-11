61883acffed9b7a20516f77331e764611dda8103
suwendra <suvendra@weblankan.lk>
2022-05-11 19:17:53 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 function App() {
+
+  var user_id = sessionStorage.getItem("user_id");
+
+  const ProtectedRoute = ({
+    isAllowed,
+    redirectPath = '/',
+    children,
+  }) => {
+    if (!isAllowed) {
+      return <Navigate to={redirectPath} replace />;
+    }
+
+    return children ? children : <Outlet />;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className=''>
+
+    {/* <Router>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/create-user' element={<Register/>}/>
+        <Route path='/dashboard' element={ <Dashboard/>}/>
+        <Route path='/show-all-users' element={<ShowUsers/>}/>
+        <Route path='/user-edit/:id' element={<UserEdit/>}/>
+        <Route path='/create-user-role' element={<CreateRole/>}/>
+        <Route path='/show-user-roles' element={<ShowRoles/>}/>
+        <Route path='/client' element={<Client/>}/>
+        <Route path='/client-list' element={<ClientList/>}/>
+        <Route path='/client-edit/:id' element={<ClientEdit/>}/>
+        <Route path='/call-list' element={<CallList/>}/>
+        <Route path='/report-call-logs' element={<CallLogsReport/>}/>
+      </Routes>
+    </Router> */}
+    <Router>
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<ProtectedRoute isAllowed={!!user_id} />}>
+
+          {/* <Route path="/dashboard" element={<Dashboard />} />
+          <Route path='/create-user' element={<Register />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/show-all-users' element={<ShowUsers />} />
+          <Route path='/user-edit/:id' element={<UserEdit />} />
+          <Route path='/create-user-role' element={<CreateRole />} />
+          <Route path='/show-user-roles' element={<ShowRoles />} />
+          <Route path='/client' element={<Client />} />
+          <Route path='/client-list' element={<ClientList />} />
+          <Route path='/client-edit/:id' element={<ClientEdit />} />
+          <Route path='/call-list' element={<CallList />} />
+          <Route path='/report-call-logs' element={<CallLogsReport />} />
+          <Route path='/logout' element={<Logout />} /> */}
+        </Route>
+      </Routes>
+    </Router>
+  </div>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const drawerWidth = 240;
+function Header() {
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
+
+    return (
+        <AppBar
+        position="fixed"
+        sx={{
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+            ml: { sm: `${drawerWidth}px` },
+        }}
+    >
+        <Toolbar>
+            <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerToggle}
+                sx={{ mr: 2, display: { sm: 'none' } }}
+            >
+                <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap component="div">
+                Responsive drawer
+            </Typography>
+        </Toolbar>
+    </AppBar>
+    );
+}
+
+export default Header
